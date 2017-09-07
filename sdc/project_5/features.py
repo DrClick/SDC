c3d41ff20a2d94e33c039f295f1b5ff372cf324b
Thomas W. Watson <drclick@mac.com>
2017-09-06 22:23:54 -0700

    adding resources

--- sdc/project_5/features.py
+++ sdc/project_5/features.py
@@ -21,9 +21,10 @@ def color(img, channel, color_space="RGB", bins=32, bins_range=(0,256)):
     return color_hist(feature_image, bins, bins_range)[channel][0]
 
 
-def HOG(img, pixels_per_cell=(8, 8), orient=9, cells_per_block=(2, 2)):
-
-    features = hog(img, orientations=orient, 
+def HOG(img, pixels_per_cell=(6, 6), orient=9, cells_per_block=(2, 2)):
+    feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    
+    features = hog(feature_image[:,:,1], orientations=orient, 
                    pixels_per_cell=pixels_per_cell, 
                    cells_per_block=cells_per_block, 
                    visualise=False, 
